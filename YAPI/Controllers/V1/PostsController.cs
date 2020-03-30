ef2b0ab9c7ece793abed769d300bacd2e2c7fb67
yavuz <yavuz@farmazon.com.tr>
2020-03-30 23:21:25 +0300

    API versioning

---
+++ YAPI/Controllers/V1/PostsController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using YAPI.Contracts.V1;
+using YAPI.Domain;
+
+namespace YAPI.Controllers.V1
+{
+    public class PostsController : Controller
+    {
+        private List<Post> posts;
+        public PostsController()
+        {
+            posts = new List<Post>();
+            for (int i = 0; i < 5; i++)
+            {
+                posts.Add(new Post { Id = Guid.NewGuid().ToString() });
+            }
+        }
+
+     
+        [HttpGet(ApiRoutes.Posts.GetAll)]
+        public IActionResult GetAll()
+        {
+            return Ok(posts);
+        }
+    }
+}
--- YAPI/Startup.cs
+++ YAPI/Startup.cs
@@ -67,12 +67,7 @@ namespace YAPI
             app.UseStaticFiles();
 
 
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
-            });
+            app.UseMvc();
         }
     }
 }
