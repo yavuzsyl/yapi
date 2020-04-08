a395958da98ea32fee25c666c199ad253369e463
yavuz <yavuz@farmazon.com.tr>
2020-04-09 02:21:06 +0300

    TestProjjjjjjjjjjjjjj

---
+++ YAPI.IntegrationTest/IntegrationTest.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using YAPI.Contracts.V1;
+using YAPI.Contracts.V1.Requests;
+using YAPI.Contracts.V1.Responses;
+using YAPI.Data;
+using YAPI.Domain;
+
+namespace YAPI.IntegrationTest
+{
+    public class IntegrationTest
+    {
+        protected readonly HttpClient testClient;
+
+        public IntegrationTest()
+        {
+            var appFactory = new WebApplicationFactory<Startup>()
+                //creating in memory database instead of using the real one
+                .WithWebHostBuilder(builder =>
+                {
+                    builder.ConfigureServices(services =>
+                    {
+                        services.RemoveAll(typeof(DataContext));
+                        services.AddDbContext<DataContext>(options =>
+                        {
+                            options.UseInMemoryDatabase("yapiIntegrationTestDB");
+                        });
+                    });
+                });
+            testClient = appFactory.CreateClient();
+        }
+
+        protected async Task AuthenticateAsync()
+        {
+            testClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", await GetJwtAsync());
+        }
+        private async Task<string> GetJwtAsync()
+        {
+            var response = await testClient.PostAsJsonAsync(ApiRoutes.Identity.Register, new RegistrationRequest
+            {
+                Email = "integration@mail.com",
+                Password = "123456It!"
+            });
+
+            var regsitrationResponse = await response.Content.ReadAsAsync<AuthenticationResult>();
+            return regsitrationResponse.Token;
+        }
+
+        protected async Task<PostResponse> CreatePostAsync(CreatePostRequest createPostRequest)
+        {
+            var response = await testClient.PostAsJsonAsync(ApiRoutes.Posts.Create, createPostRequest);
+            return (await response.Content.ReadAsAsync<PostResponse>());
+        }
+    }
+}
---
+++ YAPI.IntegrationTest/PostControllerTest.cs
@@ -0,0 +1,50 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using YAPI.Contracts.V1;
+using YAPI.Contracts.V1.Requests;
+using YAPI.Domain;
+
+namespace YAPI.IntegrationTest
+{
+    public class PostControllerTest : IntegrationTest
+    {
+        [Fact]
+        public async Task GetAll_ReturnsEmptyResponse_WithoutAnyPosts()
+        {
+            //Arange 
+            //will authenticate the client
+            await AuthenticateAsync();
+
+            //Act
+            var response = await testClient.GetAsync(ApiRoutes.Posts.GetAll);
+
+            //Assert
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+            (await response.Content.ReadAsAsync<List<Post>>()).Should().BeEmpty();
+        }
+
+
+        [Fact]
+        public async Task Get_ReturnsPost_WhenPostExists()
+        {
+            //Arange
+            await AuthenticateAsync();
+            var willBeCreatedPost = await CreatePostAsync(new CreatePostRequest { Name = "test" });
+
+            //Act
+            var response = await testClient.GetAsync(ApiRoutes.Posts.Get.Replace("{postId}", willBeCreatedPost.Id.ToString()));
+
+            //Assert
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+            var returnedPostFromGet = await response.Content.ReadAsAsync<Post>();
+            returnedPostFromGet.Id.Should().Be(willBeCreatedPost.Id);
+         }
+
+    }
+}
