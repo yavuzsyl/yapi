31728cfe66b89efb02848005802e2cd6aba63c44
yavuz <yavuz@farmazon.com.tr>
2020-04-23 01:19:18 +0300

    tagreq

---
+++ YAPI/Contracts/V1/Requests/CreateTagRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YAPI.Contracts.V1.Requests
+{
+    public class CreateTagRequest
+    {
+        public string Name { get; set; }
+    
+    }
+}
